9c284b29089b1cc41b98015230081b817774a161
Aivan Ross Anuyo <aish3n@pm.me>
2024-06-09 09:13:18 +0800

    fix size of components

--- src/main/java/com/nu_dasma/cms/swing_ui/StudentUIFrame.java
+++ src/main/java/com/nu_dasma/cms/swing_ui/StudentUIFrame.java
@@ -53,7 +53,7 @@ public class StudentUIFrame extends BaseFrame {
         TextLabel course = new TextLabel("Bachelor of fuckall", 10);
         TextLabel idNum = new TextLabel("i84937248", 10);
         TextLabel department = new TextLabel("SECA", 10);
-        TextLabel email = new TextLabel("dnuts@students.nu-narnia.edu.ph", 10);
+        TextLabel email = new TextLabel("dnuts@students.nu-narnia.edu.ph", 8);
         TextLabel rqmt = new TextLabel("Requirements", 20);
 
         JSeparator separator = new JSeparator();
@@ -73,23 +73,23 @@ public class StudentUIFrame extends BaseFrame {
 
         JTable borrowedItems = new JTable();
         borrowedItems.setModel(model);
+        borrowedItems.getTableHeader().setReorderingAllowed(false);
 
         JScrollPane borrowedItemsScroll = new JScrollPane(borrowedItems);
-        borrowedItemsScroll.revalidate();
         borrowedItemsScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
         titlePanel.setPreferredSize(new Dimension(WIDTH, (int) (HEIGHT * 0.1)));
         titlePanel.setBackground(Color.GRAY);
 
-        mainPanel.setPreferredSize(new Dimension(WIDTH - 60, (int) (HEIGHT * 1.8)));
-        mainPanel.setMinimumSize(new Dimension(WIDTH - 60, (int) (HEIGHT * 1.8)));
+        mainPanel.setPreferredSize(new Dimension(WIDTH, (int) (HEIGHT * 1.7)));
+        mainPanel.setMinimumSize(new Dimension(WIDTH, (int) (HEIGHT * 1.7)));
 
         mainPanel.setLayout(null);
         mainPanel.setBackground(Color.WHITE);
 
 
         studentInfo.setLayout(null);
-        studentInfo.setBounds(5, 5, WIDTH - 60, 150);
+        studentInfo.setBounds(5, 5, WIDTH, 150);
         studentInfo.setBackground(Color.WHITE);
         studentInfo.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
@@ -106,7 +106,7 @@ public class StudentUIFrame extends BaseFrame {
         for (String label : labelHeaders) {
             TextLabel header = new TextLabel(label, 10);
             header.setBounds(headerX, headerY + 30, 200, 30);
-            header.setFont(new Font("Arial", Font.BOLD, 15));
+            header.setFont(new Font("Arial", Font.BOLD, 12));
             headerY += 25;
             studentInfo.add(header);
         }
@@ -117,23 +117,23 @@ public class StudentUIFrame extends BaseFrame {
         studentInfo.add(idImage);
 
         matriculation.setLayout(new BorderLayout());
-        matriculation.setBounds(5, 165, WIDTH - 30, 250);
+        matriculation.setBounds(5, 165, (int) (WIDTH * 0.9), 250);
         matriculation.setBackground(Color.WHITE);
         matriculation.setBorder(new RoundedBorder(10));
 
         miniTitlePanel.setLayout(new BoxLayout (miniTitlePanel, BoxLayout.Y_AXIS));
         miniTitlePanel.setBackground(Color.WHITE);
-        miniTitlePanel.setPreferredSize(new Dimension(WIDTH - 60, (int)(250 * 0.2)));
+        miniTitlePanel.setPreferredSize(new Dimension((int) (WIDTH * 0.9), (int)(250 * 0.2)));
         miniTitlePanel.add(rqmt);
         miniTitlePanel.add(separator1);
 
-        rqGrid.setPreferredSize(new Dimension(WIDTH - 30, (int)(250 * 0.8)));
+        rqGrid.setPreferredSize(new Dimension((int) (WIDTH * 0.9), (int)(250 * 0.8)));
         rqGrid.setLayout(new GridLayout(5, 3, 1, 1));
         rqGrid.setBackground(Color.WHITE);
 
         for (int i = 0; i < requirements.length; i++) {
-            TextLabel rq = new TextLabel(requirements[i], 15);
-            rq.setFont(new Font("Arial", Font.BOLD, 15));
+            TextLabel rq = new TextLabel(requirements[i], 12);
+            rq.setFont(new Font("Arial", Font.BOLD, 12));
             rqGrid.add(rq);
 
             submitButtons[i] = new CustomButton("submit", 5, 10, 15, 15);
@@ -145,7 +145,7 @@ public class StudentUIFrame extends BaseFrame {
         matriculation.add(miniTitlePanel, BorderLayout.NORTH);
         matriculation.add(rqGrid, BorderLayout.CENTER);
 
-        financial.setBounds(5, 425, WIDTH - 30, 200);
+        financial.setBounds(5, 425, (int) (WIDTH * 0.9), 200);
         financial.setLayout(new BoxLayout(financial, BoxLayout.Y_AXIS));
         financial.setBackground(Color.WHITE);
         financial.setBorder(new RoundedBorder(10));
@@ -164,7 +164,7 @@ public class StudentUIFrame extends BaseFrame {
         financial.add(new TextLabel("Remaining Balance:", 10));
         financial.add(balanceAmount);
 
-        miscellanous.setBounds(5, 635, WIDTH - 30, 350);
+        miscellanous.setBounds(5, 635, (int) (WIDTH * 0.9), 350);
         miscellanous.setLayout(new BoxLayout(miscellanous, BoxLayout.Y_AXIS));
         miscellanous.setBackground(Color.WHITE);
         miscellanous.setBorder(new RoundedBorder(10));
@@ -180,6 +180,7 @@ public class StudentUIFrame extends BaseFrame {
 
         JScrollPane scrollPane = new JScrollPane(mainPanel);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         this.add(titlePanel, BorderLayout.NORTH);
         this.add(scrollPane, BorderLayout.CENTER);
