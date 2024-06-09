d3de555dacca267f791e0593a09f25ea39284701
Aivan Ross Anuyo <aish3n@pm.me>
2024-06-09 16:56:43 +0800

    remove cli

--- src/main/java/com/nu_dasma/cms/ui/BaseUI.java
+++
@@ -1,9 +0,0 @@
-package com.nu_dasma.cms.ui;
-
-import java.util.Scanner;
-
-public interface BaseUI {
-    public void run(Scanner scanner);
-    public void dispose();
-    public void help();
-};
